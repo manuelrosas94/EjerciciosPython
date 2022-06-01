7ad534c3c86e91293b87deb66e86c3753f6d4609
Manuel <rs.manuel94@gmail.com>
2022-06-01 12:45:42 -0500

    Initial commit

---
+++ condicionales.py
@@ -0,0 +1,27 @@
+# Declaramos una variable
+calificacion = input("Introduce tu calificacion de la AZ-900")
+
+calificacion = int(calificacion)
+
+# Pregunta si la calificacion es menor a 700
+if calificacion < 700 :
+    print("No pasaste") # Si es menor a 700, muestra el mensaje
+elif calificacion > 1000 :
+    print("La calificacion final no es mas de 1000")
+else : # Si no se cumple e; if anterior, pasa a esta linea
+    print("Felicidades, pasa por tu calificacion") # Se ejecuta si ninguno de los if se cumple
+
+# Verificador de mayoria de edad
+edad = input("Introduce tu edad")
+edad = int(edad)
+
+if edad >= 18 and edad <= 100 :
+    print("Bienvenido al ... ")
+elif edad > 100 :
+    print("Si vienes acompañado, puedes ...")
+elif edad < 0 :
+    print("Ni que fueras viajero del tiempo")
+else :
+    print("No puedes llevarte esos cigarros")
+
+# En Python no hay SWITCH CASE
---
+++ tiposVariables.py
@@ -0,0 +1,27 @@
+# String - Cadena de texto  
+from tkinter.tix import Tree
+
+
+nombre = "Manuel"
+
+# Entero - Numero sin punto
+edad = 27
+
+edads = "27"
+
+print(edad + edad)
+print(edads + edads)
+
+# Flotante - Numero con punto
+pi = 3.1416
+
+edad = float(edads) # Testeo - Transformar un tipo de variable a otro
+
+# Usa type() para saber de que tipo es la variable
+print(type(nombre), type(edad))
+
+# Boolean - Booleano - SI o NO
+legustas = False
+legustas= True
+
+print(legustas)
